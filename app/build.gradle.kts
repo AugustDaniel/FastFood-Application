e7a308cc459cc1cb6822b82f20a29698e5e107a8
pieter <pieter.degraaff@icloud.com>
2024-05-30 09:41:34 +0200

    Tuto is toegevoegd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.library)
+    implementation(files("../app/src/main/java/com/example/fastfoodlib/FastFoodLib.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
