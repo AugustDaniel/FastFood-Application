af6a0e1fac9ff617276cc1bd4bd9a78ddf332b07
pieter <pieter.degraaff@icloud.com>
2024-06-04 10:48:48 +0200

    Filter opties toegevoegd in LeaderboardActivity

--- app/src/main/java/com/example/fastfoodapplication/LeaderboardActivity.java
+++ app/src/main/java/com/example/fastfoodapplication/LeaderboardActivity.java
@@ -1,11 +1,18 @@
 package com.example.fastfoodapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -21,7 +28,10 @@ import com.fastfoodlib.util.Lap;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -34,6 +44,7 @@ public class LeaderboardActivity extends AppCompatActivity {
 
     private RecyclerView lapRecyclerView;
     private LapAdapter lapRecyclerViewAdapter;
+    private Spinner spinnerFilter;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -56,6 +67,32 @@ public class LeaderboardActivity extends AppCompatActivity {
         lapRecyclerView.setAdapter(lapRecyclerViewAdapter);
         lapRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        spinnerFilter = findViewById(R.id.activity_leaderboard_spinner_filter);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.filter_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerFilter.setAdapter(adapter);
+//        System.out.println(spinnerFilter.getSelectedItem());
+
+        spinnerFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+//                System.out.println(parent.getItemAtPosition(position));
+                int stringResourceID = getResources().getIdentifier(selectedItem, "string", getPackageName());
+
+                lapRecyclerViewAdapter.sortList(selectedItem);
+                lapRecyclerViewAdapter.notifyDataSetChanged();
+                System.out.println(stringResourceID);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+
+
         executor.execute(() -> {
             try {
                 leaderboard = ServerHandler.requestLeaderboard();
@@ -70,4 +107,5 @@ public class LeaderboardActivity extends AppCompatActivity {
             }
         });
     }
+
 }
